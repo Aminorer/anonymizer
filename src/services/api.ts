7adab9ee12e2685fd4f4252d40d67e7ba5a3912b
Amine BELAHBIB <130637695+Aminorer@users.noreply.github.com>
2025-07-30 11:18:11 +0200

    Fix analyzer dataclass and update deps

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { AnalyzeResponse, CustomEntity, Entity } from '../types/entities';
+import { AnalyzeResponse, Entity } from '../types/entities';
 
 const API_BASE_URL = '/api';
 
